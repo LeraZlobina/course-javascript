904c619175393a5fd911cc818ca01d067925c062
Lera Zlobina <leras__zlobina@mail.ru>
2021-02-25 02:37:57 +0600

    third commit

---
+++ projects/geo-review/geoReview.js
@@ -0,0 +1,77 @@
+import InteractiveMap from './interactiveMap';
+
+export default class GeoReview {
+  constructor() {
+    this.formTemplate = document.querySelector('#addFormTemplate').innerHTML;
+    this.map = new InteractiveMap('map', this.onClick.bind(this));
+    this.map.init().then(this.onInit.bind(this));
+  }
+
+  onInit() {
+    const coords = JSON.parse(localStorage.getItem('review'));
+
+    if (!coords) {
+      return;
+    }
+
+    for (const item of coords) {
+      for (let i = 0; i < item.total; i++) {
+        this.map.createPlacemark(item.coords);
+      }
+    }
+
+    document.body.addEventListener('click', this.onDocumentClick.bind(this));
+  }
+
+  createForm(coords, reviews) {
+    const root = document.createElement('div');
+    root.innerHTML = this.formTemplate;
+    const reviewList = root.querySelector('.review-list');
+    const reviewForm = root.querySelector('[data-role=review-form]');
+    reviewForm.dataset.coords = JSON.stringify(coords);
+
+    for (const item of reviews) {
+      const div = document.createElement('div');
+      div.classList.add('review-item');
+      div.innerHTML = `
+                <div>
+                    <b>${item.name}</b> [${item.place}]
+                </div>
+                <div>${item.text}</div>
+                `;
+      reviewList.appendChild(div);
+    }
+
+    return root;
+  }
+
+  onClick(coords) {
+    const list = JSON.parse(localStorage.getItem('review'));
+    const form = this.createForm(coords, list);
+    this.map.setBalloonContent(form.innerHTML);
+  }
+
+  onDocumentClick(e) {
+    if (e.target.dataset.role === 'review-add') {
+      const reviewForm = document.querySelector('[data-role=review-form]');
+      const coords = JSON.parse(reviewForm.dataset.coords);
+      const data = {
+        coords,
+        review: {
+          name: document.querySelector('[data-role=review-name]').value,
+          place: document.querySelector('[data-role=review-place]').value,
+          text: document.querySelector('[data-role=review-text]').value,
+        },
+      };
+
+      try {
+        localStorage.setItem('review', JSON.stringify(data));
+        this.map.createPlacemark(coords);
+        this.map.closeBalloon();
+      } catch (e) {
+        const formError = document.querySelector('.form-error');
+        formError.innerText = e.message;
+      }
+    }
+  }
+}
